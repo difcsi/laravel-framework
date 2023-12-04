d327f52a564b5b1dd0f531904419407f3f1a123c
difcsi <zoltan.m@tuta.io>
2023-12-04 10:05:28 +0000

    fix: honor timezone in LastDayOfMonth schedule frequency

--- src/Illuminate/Console/Scheduling/ManagesFrequencies.php
+++ src/Illuminate/Console/Scheduling/ManagesFrequencies.php
@@ -563,7 +563,7 @@ trait ManagesFrequencies
     {
         $this->dailyAt($time);
 
-        return $this->spliceIntoPosition(3, Carbon::now()->endOfMonth()->day);
+        return $this->spliceIntoPosition(3, Carbon::now($this->timezone)->endOfMonth()->day);
     }
 
     /**
